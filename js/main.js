336165a680a627d7ce45fa9f25fb8c27d6001587
Lucas Badino <lucas.badino.7@gmail.com>
2023-07-14 09:59:32 -0300

    changed

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 alert("Bienvenido al sistema de gestion \n de stock y ventas")
-
+// variable con para comfirmar ingreso al sistema
 let con = true;
+//arreglos simuladores de base de datos y pusheos de info
 const array_sales = new Array()
 let array_cars = new Array()
 array_cars.push(new Car((array_cars.length + 1), "Chevrolet", "Cruze 1.4T Ltz AT 4P", 2020, 158000))
+array_cars.push(new Car((array_cars.length + 1), "VW", "Amarok 2.0 TDI 4X2 Trendline", 2012, 10256000))
 array_cars.push(new Car((array_cars.length + 1), "Peugeot", "207 1.4T HDI Allure 5P", 2016, 256000))
 array_cars.push(new Car((array_cars.length + 1), "Toyota", "Corolla SEG 1.8 CVT", 2018, 5690333))
 
@@ -11,6 +13,7 @@ const array_custumers = new Array()
 array_custumers.push(new Customer((array_custumers.length + 1), "Lucas", "Badino", 39472312, true))
 array_custumers.push(new Customer((array_custumers.length + 1), "Pedro", "Castillo", 25369147, false))
 array_custumers.push(new Customer((array_custumers.length + 1), "Santiago", "Baigorria", 17734808, true))
+//mensaje de selector de operacion
 let mensaje = "Ingrese opcion a realizar:"
 mensaje += "\n 1- Consultar Stock"
 mensaje += "\n 2- Agregar Vehiculo"
@@ -18,21 +21,33 @@ mensaje += "\n 3- Consultar Cliente"
 mensaje += "\n 4- Agregar Clientes"
 mensaje += "\n 5- Realizar una venta"
 mensaje += "\n 6- Ventas realizadas"
-mensaje += "\n 7- Eliminar auto"
+mensaje += "\n 7- Eliminar auto del stock"
+mensaje += "\n 8- Salir"
+// mensajes para mostrar stock y clientes 
 let men_cars = "Estos son los vehiculos en stock"
 let men_cust = "Clientes: "
+
+//for each para recorrer y cargar en mensaje el stock y clientes actuales
 array_cars.forEach(e => men_cars += `\n ${e.get_info()}`)
 array_custumers.forEach(e => men_cust += `\n ${e.get_info()}`)
+
+
 if (con) {
-    flag2 = true
+    flag2 = false
     do {
         option = parseInt(prompt(mensaje))
         switch (option) {
+            // mostrar stock
             case 1: {
+                chose = parseInt(prompt("ingrese: \n 1-Ordenar por precio \n -2 Ordenar por Marca \n 3-No Ordenar"))
+                while (isNaN(chose) || chose < 1 || chose > 3) {
+                    chose = parseInt(prompt("Valor incorrecto \n ingrese: \n 1-Ordenar por precio \n -2 Ordenar por Marca \n 3-No Ordenar"))
+                }
+                order_items(chose)
                 show_stock()
                 break;
             }
-
+            // agregar vehiculo
             case 2: {
                 flag = true
                 while (flag) {
@@ -45,10 +60,12 @@ if (con) {
                 show_stock()
                 break;
             }
+            // mostrar clientes 
             case 3: {
                 show_cust()
                 break;
             }
+            // agregar cliente
             case 4: {
                 flag = true
                 while (flag) {
@@ -61,33 +78,47 @@ if (con) {
                 show_cust()
                 break;
             }
+            // realizar venta 
             case 5: {
                 sell()
                 break;
 
             }
+            // consultar ventas 
             case 6: {
-                array_sales.forEach(e => document.write(e.get_info()));
-                flag2 = false
+                if (array_sales.length > 0) {
+                    array_sales.forEach(e => document.write(e.get_info()));
+                } else {
+                    alert("no hubo ventas")
+                }
+
+                flag2 = true
 
 
                 break;
             }
+            // eliminar un vehiculo de stock
             case 7: {
                 opcion = parseInt(prompt("ingresa un valor a eliminar"))
                 delete_car(opcion)
                 show_stock()
                 break;
             }
+            // salir del programa
+            case 8: {
+                flag2 = confirm("Realmente Quiere Salir?")
+                break;
+            }
             default: {
-                flag2 = false
+                alert("valor ingresado no valido!")
+                flag2 = confirm("Quiere Salir del sistema?")
                 break;
 
             }
         }
 
 
-    } while (flag2)
+    } while (!flag2)
 }
 
 function request_data_cars() {
@@ -163,9 +194,52 @@ function sell() {
 }
 
 function delete_car(opcion) {
-    
+
     array_cars = array_cars.filter(e => e.id != opcion)
     men_cars = "Estos son los vehiculos en stock: "
     array_cars.forEach(e => men_cars += `\n ${e.get_info()}`)
+
+}
+function order_items(e) {
+    if (e == 1) {
+        array_cars.sort(function (a, b) {
+            if (a.price > b.price) {
+                return 1
+            }
+            if (a.price < b.price) {
+                return -1
+            }
+            return 0
+        })
+        men_cars = ""
+        array_cars.forEach(e => men_cars += `\n ${e.get_info()}`)
+    }
+    if (e == 2) {
+        array_cars.sort(function (a, b) {
+            if (a.brand > b.brand) {
+                return 1
+            }
+            if (a.brand < b.brand) {
+                return -1
+            }
+            return 0
+        })
+        men_cars = ""
+        array_cars.forEach(e => men_cars += `\n ${e.get_info()}`)
+
+    }
+    if (e == 3) {
+        array_cars.sort(function (a, b) {
+            if (a.id > b.id) {
+                return 1
+            }
+            if (a.id < b.id) {
+                return -1
+            }
+            return 0
+        })
+        men_cars = ""
+        array_cars.forEach(e => men_cars += `\n ${e.get_info()}`)
+    }
 
 }
