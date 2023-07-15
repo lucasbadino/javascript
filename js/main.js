dc44f716060699392283df42728ddbe7cce20a2e
lucasbadino <lucas.badino.7@gmail.com>
2023-07-14 23:16:41 -0300

    fixed problem select id sell

--- js/main.js
+++ js/main.js
@@ -1,18 +1,31 @@
 alert("Bienvenido al sistema de gestion \n de stock y ventas")
+
 // variable con para comfirmar ingreso al sistema
+
 let con = true;
+
 //arreglos simuladores de base de datos y pusheos de info
-const array_sales = new Array()
+
+let array_sales = new Array()
+let array_cars_saled = new Array()
 let array_cars = new Array()
+let array_custumers = new Array()
+
 array_cars.push(new Car((array_cars.length + 1), "Chevrolet", "Cruze 1.4T Ltz AT 4P", 2020, 158000))
+array_cars.push(new Car((array_cars.length + 1), "Toyota", "Hilux 4x2 SRV Manual", 2012, 8900000))
+array_cars.push(new Car((array_cars.length + 1), "Fiat", "Pulse 1.0T Audance", 2011, 9620000))
+array_cars.push(new Car((array_cars.length + 1), "Bmw", "Serie M4 S", 2015, 6900000))
 array_cars.push(new Car((array_cars.length + 1), "VW", "Amarok 2.0 TDI 4X2 Trendline", 2012, 10256000))
 array_cars.push(new Car((array_cars.length + 1), "Peugeot", "207 1.4T HDI Allure 5P", 2016, 256000))
 array_cars.push(new Car((array_cars.length + 1), "Toyota", "Corolla SEG 1.8 CVT", 2018, 5690333))
 
-const array_custumers = new Array()
 array_custumers.push(new Customer((array_custumers.length + 1), "Lucas", "Badino", 39472312, true))
-array_custumers.push(new Customer((array_custumers.length + 1), "Pedro", "Castillo", 25369147, false))
+array_custumers.push(new Customer((array_custumers.length + 1), "Pedro", "Zapata", 25369147, false))
+array_custumers.push(new Customer((array_custumers.length + 1), "Jose", "Almada", 41258000, true))
+array_custumers.push(new Customer((array_custumers.length + 1), "Agustin", "Martinez", 21563002, false))
+array_custumers.push(new Customer((array_custumers.length + 1), "Lorenzo", "Peralta", 10933000, true))
 array_custumers.push(new Customer((array_custumers.length + 1), "Santiago", "Baigorria", 17734808, true))
+
 //mensaje de selector de operacion
 let mensaje = "Ingrese opcion a realizar:"
 mensaje += "\n 1- Consultar Stock"
@@ -22,12 +35,16 @@ mensaje += "\n 4- Agregar Clientes"
 mensaje += "\n 5- Realizar una venta"
 mensaje += "\n 6- Ventas realizadas"
 mensaje += "\n 7- Eliminar auto del stock"
-mensaje += "\n 8- Salir"
+mensaje += "\n 8- Consultar Vehiculos Vendidos"
+mensaje += "\n 9- Salir"
+
 // mensajes para mostrar stock y clientes 
+
 let men_cars = "Estos son los vehiculos en stock"
 let men_cust = "Clientes: "
 
 //for each para recorrer y cargar en mensaje el stock y clientes actuales
+
 array_cars.forEach(e => men_cars += `\n ${e.get_info()}`)
 array_custumers.forEach(e => men_cust += `\n ${e.get_info()}`)
 
@@ -39,9 +56,9 @@ if (con) {
         switch (option) {
             // mostrar stock
             case 1: {
-                chose = parseInt(prompt("ingrese: \n 1-Ordenar por precio \n -2 Ordenar por Marca \n 3-No Ordenar"))
+                chose = parseInt(prompt("ingrese: \n 1- Ordenar por precio \n 2- Ordenar por Marca \n 3- No Ordenar"))
                 while (isNaN(chose) || chose < 1 || chose > 3) {
-                    chose = parseInt(prompt("Valor incorrecto \n ingrese: \n 1-Ordenar por precio \n -2 Ordenar por Marca \n 3-No Ordenar"))
+                    chose = parseInt(prompt("Valor incorrecto \n ingrese: \n 1- Ordenar por precio \n 2- Ordenar por Marca \n 3- No Ordenar"))
                 }
                 order_items(chose)
                 show_stock()
@@ -50,13 +67,8 @@ if (con) {
             // agregar vehiculo
             case 2: {
                 flag = true
-                while (flag) {
-                    flag = confirm("Desea agregar otro vehiculo? ")
-                    if (flag) {
-                        request_data_cars()
-                        add_cars(brand, model, year, price)
-                    }
-                }
+                request_data_cars()
+                add_cars(brand, model, year, price)
                 show_stock()
                 break;
             }
@@ -104,8 +116,23 @@ if (con) {
                 show_stock()
                 break;
             }
-            // salir del programa
+            //vehiculos vendidos
             case 8: {
+                array_sales.forEach(e => {
+                    array_cars_saled.push(new Cars_saled(array_cars_saled.length + 1, e.car))
+                })
+                if (array_cars_saled.length != 0) {
+                    men_car_saled = ""
+                    array_cars_saled.forEach((e) => men_car_saled += e.get_info())
+                    document.write(men_car_saled)
+                } else {
+
+                }
+
+                break;
+            }
+            // salir del programa
+            case 9: {
                 flag2 = confirm("Realmente Quiere Salir?")
                 break;
             }
@@ -167,18 +194,22 @@ function add_cust(name, last_name, dni, is_client) {
     alert(`Felicitaciones a agregado a: \n ${new_cust.get_info()}`)
 }
 function saled_done(new_sale) {
-    alert(`Operacion comfirmadad bajo el comprobante ${new_sale.id} \n ${car_selected.get_info()}
+    alert(`Operacion comfirmadad bajo el comprobante Nº:${new_sale.id} \n ${car_selected.get_info()}
     Vendido \n Comprado por ${cust_selected.get_info()} 
     \n El Dia ${new_sale.date} a las ${new_sale.time}`)
 }
 function sell() {
-    let car_to_sell = parseInt(prompt(`Ingrese el vehiculo a vender:\n${men_cars}`))
-    while (isNaN(car_to_sell) || car_to_sell <= 0 || car_to_sell >= array_cars.length + 1) {
+    car_to_sell = parseInt(prompt(`Ingrese el vehiculo a vender:\n${men_cars}`))
+    a = val_id(array_cars, car_to_sell)
+    while (a) {
         car_to_sell = parseInt(prompt(`Error, dato incorrecto. \nIngrese el vehiculo a vender:\n${men_cars}`))
+        a = val_id(array_cars, car_to_sell)
     }
     let cus_to_buy = parseInt(prompt(`Ingrese el cliente comprador:\n${men_cust}`))
+    a = val_id(array_custumers, cus_to_buy)
     while (isNaN(cus_to_buy) || cus_to_buy <= 0 || cus_to_buy >= array_custumers.length + 1) {
         cus_to_buy = parseInt(prompt(`Error, dato incorrecto. \nIngrese el cliente comprador:\n${men_cust}`))
+        a = val_id(array_custumers, cus_to_buy)
     }
     car_selected = array_cars.find((e) => e.id == car_to_sell)
     cust_selected = array_custumers.find((e) => e.id == cus_to_buy)
@@ -242,4 +273,12 @@ function order_items(e) {
         array_cars.forEach(e => men_cars += `\n ${e.get_info()}`)
     }
 
+}
+function val_id(array, option) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].id == option) {
+            return false;
+        }
+    }
+    return true;
 }
--- js/sales.js
+++ js/sales.js
@@ -1,6 +1,6 @@
 class Sales{
-    constructor(customer, car, id){
-        this.id = Math.round(Math.random()* 1000)
+    constructor(customer, car){
+        this.id = Math.round(Math.random()* 1000000)
         this.date = new Date().toLocaleDateString();
         this.time = new Date().toLocaleTimeString();
         this.customer = customer;
@@ -8,6 +8,17 @@ class Sales{
     }
 
     get_info(){
-        return `<br>Operacion bajo en comprobante n° ${this.id}<br> Fecha operacion ${this.date} <br> Hora operacion ${this.time} <br> Comprador: ${this.customer.get_info()} <br> Vehiculo vendido: ${this.car.get_info()}`
+        return `<br>Operacion bajo en comprobante n°: ${this.id}<br> Fecha operacion ${this.date} <br> Hora operacion ${this.time} <br> Comprador: ${this.customer.get_info()} <br> Vehiculo vendido: ${this.car.get_info()} <br> <br>`
+    }
+}
+
+class Cars_saled{
+    constructor(id, car){
+        this.id = id;
+        this.car = car
+    }
+
+    get_info(){
+        return `${this.id} - ${this.car.brand}- ${this.car.model} año :${this.car.year}`
     }
 }
